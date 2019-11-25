60f9f82c0f98e657cfe0b826fd2baa294fcfae35
dohyun <ehgus1551@naver.com>
2019-11-25 16:11:13 +0900

    database 주석

--- config/settings.py
+++ config/settings.py
@@ -88,20 +88,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-SILENCED_SYSTEM_CHECKS = [
-    'django_mysql.W002',
-]
-DATABASES = {
-    'default': {     
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'whattoeat',
-        'OPTIONS': {            
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-            'read_default_file': os.path.join(BASE_DIR, 'mydb.cnf'),
-        },
-    }
-}
+# SILENCED_SYSTEM_CHECKS = [
+#     'django_mysql.W002',
+# ]
+# DATABASES = {
+#     'default': {     
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'whattoeat',
+#         'OPTIONS': {            
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#             'read_default_file': os.path.join(BASE_DIR, 'mydb.cnf'),
+#         },
+#     }
+# }
 
 
 # Password validation
